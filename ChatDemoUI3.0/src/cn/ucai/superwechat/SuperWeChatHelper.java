ea3b3b3076449122fba2c90f43d628009bdf2f5a
sunyucc <799958317@qq.com>
2016-11-03 15:47:15 +0800

    添加1个adapter，两个fragment，4个widget

--- ChatDemoUI3.0/src/cn/ucai/superwechat/SuperWeChatHelper.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/SuperWeChatHelper.java
@@ -1253,6 +1253,7 @@ public class SuperWeChatHelper {
         isGroupAndContactListenerRegisted = false;
         
         setContactList(null);
+        setAppContactList(null);
         setRobotList(null);
         getUserProfileManager().reset();
         SuperWeChatDBManager.getInstance().closeDB();
