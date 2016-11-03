8f7967ead84bbcba412c5a8cafdf5bb9a88f7679
sunyucc <799958317@qq.com>
2016-11-03 16:27:05 +0800

    完成main布局

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/DiscoverFragment.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/DiscoverFragment.java
@@ -11,11 +11,11 @@ import cn.ucai.superwechat.R;
 /**
  * Created by clawpo on 2016/9/22.
  */
-public class DicoverFragment extends Fragment {
+public class DiscoverFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dicover, container, false);
+        return inflater.inflate(R.layout.fragment_discover, container, false);
     }
 
 //    //朋友圈点击事件
