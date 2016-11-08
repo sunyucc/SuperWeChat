7ef600206b5fa234f08b18503bc8c33de477d878
sunyucc <799958317@qq.com>
2016-11-08 15:05:23 +0800

    添加联系人的NetDao

--- ChatDemoUI3.0/src/cn/ucai/superwechat/data/NetDao.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/data/NetDao.java
@@ -75,4 +75,12 @@ public class NetDao {
                 .targetClass(String.class)
                 .execute(listener);
     }
+    public static void addUser(Context context, String username,String cname, OkHttpUtils.OnCompleteListener<String> listener){
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_ADD_CONTACT)
+                .addParam(I.User.USER_NAME,username)
+                .addParam(I.Contact.CU_NAME,cname)
+                .targetClass(String.class)
+                .execute(listener);
+    }
 }
